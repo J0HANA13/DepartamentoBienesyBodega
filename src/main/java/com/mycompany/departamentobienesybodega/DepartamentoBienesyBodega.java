3b6a1463371b4d28931eb9136ebd236e3cdc6152
J0HANA13 <johanaromeroc333@gmail.com>
2024-11-26 11:47:32 -0500

    Departamento de Bienes y Bodega

--- src/main/java/com/mycompany/departamentobienesybodega/DepartamentoBienesyBodega.java
+++ src/main/java/com/mycompany/departamentobienesybodega/DepartamentoBienesyBodega.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.departamentobienesybodega;
 
-/**
- *
- * @author KATY
- */
 public class DepartamentoBienesyBodega {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Bienvenidos a Departamento de Bienes y Bodega");
     }
 }
